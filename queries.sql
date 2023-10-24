288ed9c910d82a7e37b6e99a25f630a103f6d4a2
siddghosh108 <siddhartha@anixsoft.co.in>
2023-10-24 18:43:08 +0530

    Uploading files for the branch query-and-update-animals-table

--- queries.sql
+++ queries.sql
@@ -1,17 +1,45 @@
-/*Queries that provide answers to the questions from all projects.*/
-
 SELECT * FROM animals WHERE name LIKE '%mon';
-
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
-
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-
 SELECT * FROM animals WHERE neutered = true;
-
 SELECT * FROM animals WHERE name != 'Gabumon';
-
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN; 
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT savepoint;
+SELECT * FROM animals;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+--QUESTIONS 
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered , SUM(escape_attempts) AS total_attemps FROM animals GROUP BY neutered ORDER BY total_attemps DESC LIMIT 1;
+SELECT species , MIN(weight_kg) AS minimum, MAX(weight_kg) AS maximum FROM animals GROUP BY species;
+SELECT species , AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-/* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
@@ -7,3 +7,5 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
