4d40ae831f5afcdc0e0d0e50349e5ab007d9ded7
siddghosh108 <131663359+siddghosh108@users.noreply.github.com>
2023-10-25 11:00:08 -0700

    Upload files in branch query-multiple-tables of the vet-clinic project

--- queries.sql
+++ queries.sql
@@ -19,7 +19,6 @@ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 SELECT * FROM animals;
 COMMIT;
-SELECT * FROM animals;
 
 BEGIN; 
 DELETE FROM animals;
@@ -44,3 +43,18 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered , SUM(escape_attempts) AS total_attemps FROM animals GROUP BY neutered ORDER BY total_attemps DESC LIMIT 1;
 SELECT species , MIN(weight_kg) AS minimum, MAX(weight_kg) AS maximum FROM animals GROUP BY species;
 SELECT species , AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+--QUERIES USING JOIN
+SELECT a.name FROM animals a JOIN owners o ON a.owners_id=o.id WHERE o.full_name='Melody Pond';
+
+SELECT a.name FROM animals a JOIN species s ON a.species_id = s_id WHERE s.name='Pokemon';
+
+SELECT owners.full_name,animals.name FROM owners LEFT JOIN animals ON owners.id=animals.owners_id;
+
+SELECT species.name,COUNT(animals.id) FROM animals  JOIN species ON animals.species_id = species.id GROUP BY species.name;
+
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id JOIN species ON animals.species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+SELECT owners.full_name FROM animals JOIN owners ON animals.owners_id = owners.id GROUP BY owners.full_name ORDER BY COUNT(animals.id) DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -9,3 +9,20 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owners_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id) REFERENCES owners(id);
